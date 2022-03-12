6532971a21c932b7dfafd1eaa887f30455d58c05
Vinoo <ivinoop@gmail.com>
2022-03-12 18:20:08 +0530

    edit

--- block-BNaacd/user.js
+++ block-BNaacd/user.js
@@ -4,5 +4,5 @@ const Schema  = mongoose.Schema;
 let userSchema = ({
   name: {type: String, lowercase: true},
   email: String,
-  age: {type: Number, min: 0}
+  age: {type: Number, default: 0}
 })
